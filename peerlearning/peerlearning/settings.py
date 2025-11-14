eac71673ebab27b66e37ae56950a57d384fbd3a5
LYCIA <HACERILHAN@HOTMAIL.FR>
2025-11-14 21:34:25 +0800

    database svg is added. still needs to be updated. this is for a quick setup.

--- peerlearning/peerlearning/settings.py
+++ peerlearning/peerlearning/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
